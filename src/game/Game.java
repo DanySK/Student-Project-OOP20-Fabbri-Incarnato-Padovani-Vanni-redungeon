87e09baaa61d60b7f3946a000a414980c5472165
LuKe2_Ink <luigi.incarnato@studio.unibo.it>
2021-03-30 17:19:33 +0200

    Re-done it

---
+++ src/game/Game.java
@@ -0,0 +1,104 @@
+package game;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+
+//nabi
+
+public class Game extends Canvas implements Runnable{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8647713295702872480L;
+	
+	public static final int WIDTH =1600, HEIGHT=WIDTH/12*9;
+	public static final int MAPW=1600,MAPH=HEIGHT;
+	
+	private Thread thread; 
+	
+	private boolean running= false;
+	
+	
+	private Handler handler;
+
+	public Game() {
+		handler=new Handler();
+		//new Window(WIDTH,HEIGHT,"IsekaiPon",this); 	WIP ;) ;)
+	}
+	
+	public synchronized void start() {
+		thread= new Thread(this);
+		thread.start();
+		running = true;
+		
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running=false;
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void run() {
+		long lastTime= System.nanoTime();
+		double numberofticks=60.0;
+		double nanoseconds= 1000000000/numberofticks;
+		double delta=0;
+		long timer= System.currentTimeMillis();
+		int frames=0;
+		while(running) {
+			long now= System.nanoTime();
+			delta+=(now-lastTime)/nanoseconds;
+			lastTime=now;
+			while(delta>=1) {
+				tick();
+				delta=0;
+			}
+			if(running) {
+				render();
+			}
+			frames++;
+			
+			if(System.currentTimeMillis()-timer>1000) {
+				timer+=1000;
+				frames=0;
+			}
+		}
+		stop();
+		
+	}
+	
+	private void tick() {
+		handler.tick();
+	}
+	
+	private void render() {
+		BufferStrategy bs= this.getBufferStrategy();
+		if(bs==null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics2D g= (Graphics2D) bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		handler.render(g);
+		
+		g.dispose();
+		bs.show();
+	}
+	public static void main(String[] args) {
+		new Game();
+	}
+
+}
---
+++ src/mapandtiles/tiletype.java
@@ -0,0 +1,11 @@
+package mapandtiles;
+
+public enum tiletype {
+	
+	OFF,
+	ON,
+	Heal,
+	Exit,
+	Occupied;
+}
+ 
