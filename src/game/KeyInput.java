3038b3dc629b62686ea5c2a2a51808ab0a34cc74
MatVan99ita <matteo.vanni2@studio.unibo.it>
2021-04-01 22:09:48 +0200

    Added KeyReleased function to KeyInput

--- src/game/KeyInput.java
+++ src/game/KeyInput.java
@@ -33,7 +33,24 @@ public class KeyInput extends KeyAdapter {
 		}
 	}
 	
-	public void keyReleased() {
-		
+	public void keyReleased(KeyEvent e) {
+		int key=e.getKeyCode();
+		for(int i=0; i<handler.object.size();i++) {
+			GameObject tempobj=handler.object.get(i);
+			if(tempobj.getID()!=ID.NPC) {
+				if(key==KeyEvent.VK_W) {
+					tempobj.setvelY(0);
+				}
+				if(key==KeyEvent.VK_A) {
+					tempobj.setvelX(0);
+				}
+				if(key==KeyEvent.VK_S) {
+					tempobj.setvelY(0);
+				}
+				if(key==KeyEvent.VK_D) {
+					tempobj.setvelX(0);
+				}
+			}
+		}
 	}
 }
