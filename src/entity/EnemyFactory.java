b497a51060b32d8cb0a376691e2535928648e82e
LuKe2_Ink <luigi.incarnato@studio.unibo.it>
2021-04-07 15:10:09 +0200

    Update EnemyFactory.java

--- src/entity/EnemyFactory.java
+++ src/entity/EnemyFactory.java
@@ -10,11 +10,12 @@ import mapandtiles.AbsFloor;
 
 public class EnemyFactory  {
 	
-public Enemy normalEnemy(int x, int y, ID id, int level, int hp, int attack, int magic_attack, int defence,AbsFloor floor, Player p)throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-	return new Enemy( x,  y,  id,  level,  hp,  attack,  magic_attack,  defence,  floor, p);
-}
-public Boss commonBoss(int x, int y, ID id, int level, int hp, int attack, int magic_attack, int defence,AbsFloor floor, Player p) throws IOException, LineUnavailableException, UnsupportedAudioFileException{
-	return new Boss( x,  y,  id,  level,  hp,  attack,  magic_attack,defence, floor, p);
-}
+	public Enemy normalEnemy(int x, int y, ID id, int level, int hp, int attack, int magic_attack, int defence,AbsFloor floor, Player p)throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+		return new Enemy( x,  y,  id,  level,  hp,  attack,  magic_attack,  defence,  floor, p);
+	}
+	
+	public Boss commonBoss(int x, int y, ID id, int level, int hp, int attack, int magic_attack, int defence,AbsFloor floor, Player p) throws IOException, LineUnavailableException, UnsupportedAudioFileException{
+		return new Boss( x,  y,  id,  level,  hp,  attack,  magic_attack,defence, floor, p);
+	}
 
 }
