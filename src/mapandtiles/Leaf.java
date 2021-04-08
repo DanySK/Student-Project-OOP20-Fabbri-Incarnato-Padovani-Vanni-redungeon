7278a5235ebd88d5ce0dee4934cec92d0a287665
Francesco <francesco.padovani2@studio.unibo.it>
2021-04-08 14:00:19 +0200

    numbers

--- src/mapandtiles/Leaf.java
+++ src/mapandtiles/Leaf.java
@@ -158,8 +158,8 @@ public class Leaf{
 
     	halls = new Vector<Rectangle>();
 
-    	Point point1 = new Point((int)Math.random()*(l.width-l.x-3)+l.x, (int)Math.random()*(l.height-3-l.y)+l.y);
-    	Point point2 = new Point((int)Math.random()*(r.width-3-r.x)+r.x, (int)Math.random()*(r.height-3-r.y)+r.y);
+    	Point point1 = new Point((int)Math.random()*(l.width-l.x-3)+l.x+1, (int)Math.random()*(l.height-3-l.y)+l.y+1);
+    	Point point2 = new Point((int)Math.random()*(r.width-3-r.x)+r.x+1, (int)Math.random()*(r.height-3-r.y)+r.y+1);
 
     	int w = point2.x - point1.x;
     	int h = point2.y - point1.y;
