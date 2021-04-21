d35d551635ac67aecff8bbdea028cb8739438037
MatVan99ita <matteo.vanni2@studio.unibo.it>
2021-04-21 17:03:54 +0200

    Added comments to utilities package

--- src/utilities/AABB.java
+++ src/utilities/AABB.java
@@ -1,12 +1,28 @@
 package utilities;
 import java.awt.Point;
 
+/**
+ * 
+ * @author Francesco Padovani
+ * @author Luigi Incarnato
+ * @author Leroy Fabbri
+ * @author Matteo Vanni
+ *
+ * @see java.awt.Point
+ *
+ */
 public class AABB {
     private Point pos;
     private int w;
     private int h;
     private int size;
 
+    /**
+     * Constructor
+     * @param p	starting point of the box
+     * @param w	box width
+     * @param h	box height
+     */
     public AABB(Point p,int w,int h) {
         this.pos=p;
         this.w=w;
@@ -14,15 +30,46 @@ public class AABB {
         this.size=Math.max(w, h);
     }
 
-
+    /**
+     * 
+     * @return position point
+     */
     public Point getpos() {return pos;}
+    /**
+     * 
+     * @return box size
+     */
     public int getsize() {return size;}
+    /**
+     * 
+     * @return horizontal position
+     */
     public int getX() {return pos.x;}
+    /**
+     * 
+     * @return vertical position
+     */
     public int getY() {return pos.y;}
+    /**
+     * Used in horizontal velocity movement
+     * @param sum 
+     */
     public void sumX(int sum) {this.pos.x+=sum;}
+    /**
+     * Used in vertical velocity movement
+     * @param sum
+     */
     public void sumY(int sum) {this.pos.y+=sum;}
+    /**
+     * Set box position
+     * @param p position point
+     */
     public void setpos(Point p) {this.pos=p;}
-
+    /**
+     * Used as control for hit on box 
+     * @param box
+     * @return
+     */
     public boolean collides(AABB box) {
     	
     	if(this.pos.x< (box.pos.x+box.w) && this.pos.x+this.w>box.pos.x && this.pos.y<box.pos.y+box.h && this.pos.y+this.h>box.pos.y)    		
--- src/utilities/SpriteSheet.java
+++ src/utilities/SpriteSheet.java
@@ -1,16 +1,34 @@
 package utilities;
 
 import java.awt.image.BufferedImage;
-
+/**
+ * @author Francesco Padovani
+ * @author Luigi Incarnato
+ * @author Leroy Fabbri
+ * @author Matteo Vanni
+ *	
+ * @see java.awt.Image.BufferedImage
+ */
 public class SpriteSheet {
 
 	private BufferedImage image;
-	
+	/**
+	 * Set the sprite sheet image
+	 * @param image
+	 */
 	public SpriteSheet(BufferedImage image)
 	{
 		this.image = image;
 	}
 	
+	/**
+	 * Work only for sprite sheet
+	 * @param col horizontal image tile position 
+	 * @param row vertical image tile position
+	 * @param width
+	 * @param height
+	 * @return the selected tile from the sheet
+	 */
 	public BufferedImage grabImage(int col, int row, int width, int height)
 	{
 		BufferedImage img = image.getSubimage((col*width)-width, (row*height)-height, width, height);
