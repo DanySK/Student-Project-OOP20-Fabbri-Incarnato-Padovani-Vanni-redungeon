e85cb9ee98a64403b7babcfa5c234e33011ec42f
LuKe2_Ink <luigi.incarnato@studio.unibo.it>
2021-04-24 03:01:21 +0200

    update

--- src/test/ReDungonTest.java
+++ src/test/ReDungonTest.java
@@ -11,6 +11,7 @@ import entity.Player;
 import game.CombatSystem;
 import game.Game;
 import game.Handler;
+import game.ID;
 import java.awt.AWTException;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
@@ -26,160 +27,126 @@ import menu.Menu;
 import utilities.AABB;
 
 public class ReDungonTest {
-	
-	public Boss boss;
-	public BossFloor bFloor;
-	public Floor floor;
-	public CombatSystem combat;
-	public Player player;
-	public AABB box;
-	public Menu menu;
-	public Game game;
-	public Enemy enemy;
-	public KeyEvent key;
-	public Handler handler;
-	List<AABB> collisions;
-	
-	static int level = 1;
-	public static int mapWidth=2000;
-	public static int mapHeight=2000;
-	static int screenWidth = 960;
-	static int screenHeight = 720;
-	static int posX=0;
-	static int posY=0;
-	
+  public Boss boss;
+  public BossFloor bFloor;
+  public Floor floor;
+  public CombatSystem combat;
+  public Player player;
+  public AABB box;
+  public Menu menu;
+  public Game game;
+  public Enemy enemy;
+  public KeyEvent key;
+  public Handler handler;
+  List<AABB> collisions;
+
+  static int level = 1;
+  public static int mapWidth = 2000;
+  public static int mapHeight = 2000;
+  static int screenWidth = 960;
+  static int screenHeight = 720;
+  static int posX = 0;
+  static int posY = 0;
+
 	@org.junit.Before
-	public void initAll() throws IOException, LineUnavailableException, UnsupportedAudioFileException, AWTException {
-		box=new AABB(new Point(), mapWidth, mapHeight);
-		floor = new Floor(level, mapWidth, mapHeight, screenWidth, screenHeight);
-		
-		collisions = new ArrayList<AABB>();
-		
-		bFloor = new BossFloor(level, mapWidth, mapHeight, screenWidth, screenHeight);
-		combat = new CombatSystem(0);
-		
-		player = new Player(
-				0, 
-				0, 
-				ID.Player, 
-				combat, 
-				1, 
-				100, 
-				10, 
-				10, 
-				10, 
-				floor);
-		
-		collisions.add(player.getBox());
+public void initAll() throws IOException, LineUnavailableException, 
+    UnsupportedAudioFileException, AWTException {
+    box = new AABB(new Point(), mapWidth, mapHeight);
+    floor = new Floor(level, mapWidth, mapHeight, screenWidth, screenHeight);
+    collisions = new ArrayList<AABB>();
+    bFloor = new BossFloor(level, mapWidth, mapHeight, screenWidth, screenHeight);
+    combat = new CombatSystem(0);
+    player = new Player(0, 0, ID.Player, combat, 1, 100, 10, 10, 10, floor);
+    
+    collisions.add(player.getBox());
 
-		boss = new Boss(
-				32, 
-				32, 
-				ID.Boss, 
-				combat, 
-				level, 
-				bFloor, 
-				player);
-		
-		enemy=new Enemy(
-				32, 
-				32, 
-				ID.Enemy, 
-				combat, 
-				level, 
-				floor, 
-				player);
+    boss = new Boss(32, 32, ID.Boss, combat, level, bFloor, player);
 
-		floor.placeEntity(player);
+    enemy = new Enemy(32, 32, ID.Enemy, combat, level, floor, player);
+
+    floor.placeEntity(player);
 
 		floor.placeEntity(enemy);
 
 		floor.placeEntity(boss);
-		
+
 		combat.addBoss(boss);
 		combat.addEnemy(enemy);
 		combat.addPlayer(player);
-		
-		game = new Game(screenWidth,screenHeight,mapWidth,mapHeight, Difficulty.Easy, 0.0, 0.0);
-		key = new KeyEvent(game, KeyEvent.KEY_PRESSED, 
-				System.currentTimeMillis(), 0,  
-				KeyEvent.VK_1,'0');
+
+		game = new Game(screenWidth, screenHeight, mapWidth, mapHeight, Difficulty.Easy, 0.0, 0.0);
+		key = new KeyEvent(game, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_1, '0');
 	}
-	
+
 	@org.junit.Test
 	public void BossTest() {
-		
-		/*the boss exp increase ten by ten*/
+
+		/* the boss exp increase ten by ten */
 		assertEquals(70, boss.getExpGuaranteed());
 		boss.setLevel(2);
-		boss.setExpGuaranteed(60); //by costructor it always have 30 + (leve*10)
+		boss.setExpGuaranteed(60); // by costructor it always have 30 + (leve*10)
 		assertEquals(80, boss.getExpGuaranteed());
-		
+
 		enemy.setLevel(1);
-		/*boss by costructor has specif stats depending on floor and player level 
-		 * at the start of the bossfloor it has the same defence as the attack
-		 * of the player
-		 * */
+		/*
+		 * boss by costructor has specif stats depending on floor and player level at
+		 * the start of the bossfloor it has the same defence as the attack of the
+		 * player
+		 */
 		assertEquals(510, boss.getMax_hp());
-		assertEquals(player.getAttack(),
-				boss.getDefence());
+		assertEquals(player.getAttack(), boss.getDefence());
 		boss.setMax_hp(500);
 		boss.setLevel(2);
 		boss.augmStat();
 		assertEquals(520, boss.getMax_hp());
-		assertEquals(player.getAttack(),
-				boss.getDefence());
+		assertEquals(player.getAttack(), boss.getDefence());
 	}
-	
+
 	@org.junit.Test
 	public void EnemyTest() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-		
-		/*the enemy exp increase ten by ten getting to next levels*/
+
+		/* the enemy exp increase ten by ten getting to next levels */
 		assertEquals(40, enemy.getExpGuaranteed());
 		enemy.setLevel(2);
-		enemy.setExpGuaranteed(30); //by costructor it always have 30 + (leve*10)
+		enemy.setExpGuaranteed(30); // by costructor it always have 30 + (leve*10)
 		assertEquals(50, enemy.getExpGuaranteed());
-		
 
 		enemy.setLevel(1);
-		/*enemy by costructor has specif stats depending on floor and player level */
+		/* enemy by costructor has specif stats depending on floor and player level */
 		assertEquals(110, enemy.getMax_hp());
-		assertEquals((player.getDefence()/2)+enemy.getLevel(),
-				enemy.getDefence());
+		assertEquals((player.getDefence() / 2) + enemy.getLevel(), enemy.getDefence());
 		enemy.setMax_hp(100);
 		enemy.setLevel(2);
 		enemy.augmStat();
 		assertEquals(120, enemy.getMax_hp());
-		assertEquals((player.getDefence()/2)+enemy.getLevel(),
-				enemy.getDefence());
-		
-		
+		assertEquals((player.getDefence() / 2) + enemy.getLevel(), enemy.getDefence());
+
 		enemy.setHp(0);
 		assertTrue(enemy.isDead());
 	}
-	
+
 	@org.junit.Test
-	public void PlayerTest(){
-		
+	public void PlayerTest() {
+
 		assertFalse(player.isDead());
-		
-		player.addExp(60); //level up beacuse the player need 50 exp
+
+		player.addExp(60); // level up beacuse the player need 50 exp
 		assertEquals(2, player.getLevel());
-		player.addExp(50); //the player's experience need 75 for the next level
-		/*now the player has 60 to 75 exp*/
+		player.addExp(50); // the player's experience need 75 for the next level
+		/* now the player has 60 to 75 exp */
 		assertNotEquals(3, player.getLevel());
-		
-		/* the player level up and the stats increase*/
-		assertTrue(player.getMax_hp()>100);
-		assertTrue(player.getAttack()>10);
-		assertTrue(player.getMagic_attack()>10);
-		assertTrue(player.getDefence()>10);
-		
-		/*try to simulate the keys pressed on the
-		 * keyboard
+
+		/* the player level up and the stats increase */
+		assertTrue(player.getMax_hp() > 100);
+		assertTrue(player.getAttack() > 10);
+		assertTrue(player.getMagic_attack() > 10);
+		assertTrue(player.getDefence() > 10);
+
+		/*
+		 * try to simulate the keys pressed on the keyboard
 		 */
-		
-		/*movement*/
+
+		/* movement */
 		key.setKeyCode(KeyEvent.VK_W);
 		player.input(key, collisions);
 		assertEquals(Direction.Up, player.getDirection());
@@ -192,31 +159,29 @@ public class ReDungonTest {
 		key.setKeyCode(KeyEvent.VK_D);
 		player.input(key, collisions);
 		assertEquals(Direction.Right, player.getDirection());
-		/*now the player done 4 moves*/
-		
-		/*attack*/
+		/* now the player done 4 moves */
+
+		/* attack */
 		key.setKeyCode(KeyEvent.VK_J);
 		player.input(key, collisions);
 		assertTrue(player.isAttacking());
 		key.setKeyCode(KeyEvent.VK_K);
 		player.input(key, collisions);
 		assertTrue(player.isMagicAttacking());
-		/*the player only have one magic attack at the start
-		 * of the game, used one it remains 0 magic attack
+		/*
+		 * the player only have one magic attack at the start of the game, used one it
+		 * remains 0 magic attack
 		 */
 		assertEquals(0, player.getSpells());
-		
+
 		player.setHp(0);
 		assertTrue(player.isDead());
 	}
-	
+
 	@org.junit.Test
 	public void CombatSystemTest() {
 		combat.damagePlayer("enemy", true);
-		
+
 	}
-	
-	
-	
-	
+
 }
